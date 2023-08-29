cd75620c0041d57c82cbf1d832ab8a9edb799ef9
Boris Volkov <bvolkov@web3tech.ru>
2023-08-29 20:05:51 +0300

    Fixes

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
         element: <MainPage/>,
         children: [
             {
-                path: "/",
+                path: "*",
                 element: <Pools />,
                 default: true,
             },
