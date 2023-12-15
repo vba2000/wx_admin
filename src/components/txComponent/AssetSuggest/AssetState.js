6d07e5018491aadd0ce6a3fa2bbe78574013608b
Boris Volkov <bvolkov@web3tech.ru>
2023-12-15 18:07:06 +0300

    Add assetSort

--- src/components/txComponent/AssetSuggest/AssetState.js
+++ src/components/txComponent/AssetSuggest/AssetState.js
@@ -1,6 +1,42 @@
 import {useCallback, useMemo, useState} from "react";
 import {checkTransactionId, fetchAssets} from "../../../services";
 
+const assetWeight = (asset, text) => {
+    const { id, asset: fullAsset, ticker = '', assetName = '' } = asset;
+    const { name } = fullAsset;
+
+    const k = (ticker || assetName || name).length;
+
+    if (!text) {
+        return 0;
+    }
+
+    const lowerText = text.toLowerCase();
+
+    if (id === text) {
+        return 0;
+    }
+
+    if (ticker.toLowerCase() === lowerText) {
+        return 0;
+    }
+
+    if (assetName.toLowerCase() === lowerText) {
+        return 1;
+    }
+
+    if (name.toLowerCase() === lowerText) {
+        return  2;
+    }
+
+    let acc = 0;
+
+    acc += (ticker.toLowerCase().indexOf(lowerText) + 1) || 1000000000;
+    acc += (assetName.toLowerCase().indexOf(lowerText) + 1) * 100 || 1000000000;
+    acc += (name.toLowerCase().indexOf(lowerText) + 1) * 100000 || 1000000000;
+    return acc * k;
+}
+
 
 const getAssetById = async (assetId = '', assets) => {
     const item = assets[assetId || 'WAVES'];
@@ -37,9 +73,9 @@ export const AssetState = (assets) => {
     }, []);
 
     const onSuggest = useCallback(async (e) => {
-        const input = (e.target.value.toString() || '').toLowerCase();
+        const input = (e.target.value.toString() || '');
         setIsLoading(true);
-        const asset = await getAssetById(e.target.value.toString() || '', assets);
+        const asset = await getAssetById(input, assets);
         if (asset) {
             assets[asset.id] = asset;
         }
@@ -49,11 +85,11 @@ export const AssetState = (assets) => {
 
     const filteredAsset = useMemo(() => {
         return Object.entries(assets).filter(([id, asset]) => {
-            const assetId = id.toLowerCase();
+            const text = suggest.toLowerCase();
             const {ticker = '', assetName = ''} = asset;
             const name = asset.asset.name.toLowerCase();
-            return ticker.toLowerCase().includes(suggest) || assetName.toLowerCase().includes(suggest) || name.includes(suggest) || assetId.includes(suggest);
-        });
+            return ticker.toLowerCase().includes(text) || assetName.toLowerCase().includes(text) || name.includes(text) || id === suggest;
+        }).sort(([, assetA], [, assetB]) => assetWeight(assetA, suggest) - assetWeight(assetB, suggest));
     }, [assets, suggest]);
 
     const state = useMemo(() => {
